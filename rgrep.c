f3c7a689a5e74919b612f6c5623bae909abc9d2f
Denis Vashchenko <denisvashchenko@gmail.com>
2018-03-15 22:52:33 -0700

    implement backslash final

--- rgrep.c
+++ rgrep.c
@@ -3,13 +3,14 @@
 #define MAXSIZE 4096
 
 /**
- * You can use this recommended helper function 
+ * You can use this recommended helper function
  * Returns true if partial_line matches pattern, starting from
  * the first char of partial_line.
  */
 int matches_leading(char *partial_line, char *pattern) {
-  // Implement if desire 
+  // Implement if desire
 	int i = 0;
+	int j = 0;
 	int dot = 1;
 	int plus = 1;
 	int question = 1;
@@ -19,13 +20,19 @@ int matches_leading(char *partial_line, char *pattern) {
 	char stringCheck[strlen(pattern)];
 
 	while(i < strlen(pattern)){
-		
-		if(pattern[i] == '\'){
+
+		if(pattern[i] == '\\'){
 		   char escape = pattern[i+1];
 		   stringCheck[i] = escape;
 		   i++;
+
+			 for(j = 0; j <= strlen(partial_line); j++){
+					if(partial_line[j] == escape){
+	 				return 1;
+			}
 		}
-		   
+	}
+
 		else if(pattern[i] == '.'){
 			dot = 0;
 
@@ -42,7 +49,7 @@ int matches_leading(char *partial_line, char *pattern) {
 			char end1 = pattern[i+j];
 
 			i = i + j;
-			
+
 			if((partial_line[0] == start && partial_line[j] == end)){
 
 				dot = 1;
@@ -54,7 +61,7 @@ int matches_leading(char *partial_line, char *pattern) {
 					dot = 1;
 			}
 			else if(start != '\000' && end == '\000'){
-				
+
 				if(partial_line[0] == start)
 					dot =  1;
 			}
@@ -70,7 +77,7 @@ int matches_leading(char *partial_line, char *pattern) {
 
 			int j;
 			for(j = 0; j <= strlen(partial_line); j++){
-				
+
 				if(preceding == '.'){
 					dot = 1;
 					preceding = '\000';
@@ -87,7 +94,7 @@ int matches_leading(char *partial_line, char *pattern) {
 			char arr2[strlen(pattern)];
 			int j = 0;
 			int arrcount = 0;
-			int arrcount2 = 0; 
+			int arrcount2 = 0;
 			while(j < strlen(pattern)){
 				if(j == i-1){
 					arr2[arrcount2] = pattern[j];
@@ -143,8 +150,8 @@ int matches_leading(char *partial_line, char *pattern) {
 }
 
 /**
- * You may assume that all strings are properly null terminated 
- * and will not overrun the buffer set by M     AXSIZE 
+ * You may assume that all strings are properly null terminated
+ * and will not overrun the buffer set by M     AXSIZE
  *
  * Implementation of the rgrep matcher function
  */
@@ -188,5 +195,3 @@ int main(int argc, char **argv) {
 
 	return 0;
 }
-
-
